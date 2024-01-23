c84753ad22d9ebea76a7af12c68f4030e40bcc33
kevin <kevindbaik@gmail.com>
2024-01-22 22:28:21 -0800

    Created add method for Forager

--- src/main/java/learn/foraging/data/ForagerRepository.java
+++ src/main/java/learn/foraging/data/ForagerRepository.java
@@ -2,9 +2,12 @@ package learn.foraging.data;
 
 import learn.foraging.models.Forager;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ForagerRepository {
+    Forager add(Forager forager) throws IOException;
+
     Forager findById(String id);
 
     List<Forager> findAll();
